ca507c98787332897854dfb88edf2d2a0f6a9071
Wepea0 <buntugu.adamwaba@ashesi.edu.gh>
2023-11-22 05:01:57 +0000

    Added the rest of the relevant task-specific model APIs

--- SWE_Final/LLMApi.py
+++ SWE_Final/LLMApi.py
@@ -15,7 +15,7 @@ def index():
 
 #Send the LLM request, receive the response, write it into a file 
 @app.route('/general_request', methods=['POST'])
-def sendLLM_request():
+def send_general_request():
     prompt = json.loads(request.data)
     response = requests.post(
         "https://api.ai21.com/studio/v1/j2-mid/complete",
@@ -73,6 +73,85 @@ def sendLLM_request():
 
     # Return response data to client
     return jsonify(response_data["completions"][0]["data"]["text"]), 200
+
+@app.route('/grammar_request', methods=['POST'])
+def send_grammar_request():
+    prompt = json.loads(request.data)
+    
+    response = requests.post(
+        "https://api.ai21.com/studio/v1/gec",
+        headers={
+            "Authorization": "Bearer 7X7mvKNPEJ4eXieVoFsTISA2p0yGumA2",
+            "Content-Type": "application/json",
+            "accept": "application/json"
+        },
+        data = json.dumps(prompt)
+    )
+
+    #TODO - Retrieve suggestions and locations in an array form?
+
+    # Process response
+    response_data = response.json()
+
+    ##Add error handling
+
+    # Return response data to client
+    return jsonify(response_data), 200
+
+
+@app.route("/improvement_request", methods = ["POST"])
+def send_improvement_request():
+    prompt = json.loads(request.data)
+    
+    response = requests.post(
+        "https://api.ai21.com/studio/v1/improvements",
+        headers={
+            "Authorization": "Bearer 7X7mvKNPEJ4eXieVoFsTISA2p0yGumA2",
+            "Content-Type": "application/json",
+            "accept": "application/json"
+        },
+        data = json.dumps(prompt)
+    )
+
+    #TODO - How to present and handle responses
+
+    # Process response
+    response_data = response.json()
+
+    ##Add error handling
+
+    # Return response data to client
+    return jsonify(response_data), 200
+
+
+
+
+@app.route("/segment_summarize", methods = ["POST"])
+def send_segment_summarization_request():
+    prompt = json.loads(request.data)
+    
+    response = requests.post(
+        "https://api.ai21.com/studio/v1/summarize-by-segment",
+        headers={
+            "Authorization": "Bearer 7X7mvKNPEJ4eXieVoFsTISA2p0yGumA2",
+            "Content-Type": "application/json",
+            "accept": "application/json"
+        },
+        data = json.dumps(prompt)
+    )
+
+    #TODO - How to present and handle responses
+
+    # Process response
+    response_data = response.json()
+
+    ##Add error handling
+
+    # Return response data to client
+    return jsonify(response_data), 200
+
+
+
 
 
     
