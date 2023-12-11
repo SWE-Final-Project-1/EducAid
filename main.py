07f7cbcd65563b9b6228d9c1ccc56cc01534ef42
Wepea0 <buntugu.adamwaba@ashesi.edu.gh>
2023-12-11 19:46:20 +0000

    Added port info for deployment

--- main.py
+++ main.py
@@ -488,4 +488,6 @@ def edit_csv(file_object, school, student_class):
 
 
 
-app.run(debug=True) 
+# app.run(debug=True) 
+if __name__ == "__main__":
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
